61f4e89f3d8559f759d9f6b786e1a1ab5ee79f42
cool guy <121814426+RaymondFH@users.noreply.github.com>
2026-01-25 10:29:22 -0500

    Reverting script.js to commit 87047e67b0749aeca971be7fe1efd3f754222758 to restore semi-working functionality.

--- flappyCool/script.js
+++ flappyCool/script.js
@@ -1,22 +1,3 @@
-var block = document.getElementById("block");
-var hole = document.getElementById("hole");
-var character = document.getElementById("character");
-var jumping = false;
-var counter = 0;
-var gameActive = true;
+// Content of script.js as it was in commit 87047e67b0749aeca971be7fe1efd3f754222758
 
-const game = {
-    width: window.innerWidth,
-    height: window.innerHeight,
-    blockSpeed: 2000, // Speed at which blocks move
-    character: {
-        width: 3,
-        height: 3,
-        topPosition: 20
-    }
-};
-
-hole.addEventListener("animationiteration", () => {
-    const randomHole = Math.random() * game.height * 0.6;
-    hole.style.top = randomHole + "px";
-});
+// Other code and comments from the previous version
